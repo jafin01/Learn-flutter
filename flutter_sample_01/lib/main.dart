4ea63ed581df88e24eb34554c71ac66eda17c34f
Jafin Jahfar <jafinjahfar@gmail.com>
2023-05-29 16:09:42 +0530

    widget text containers

--- flutter_sample_01/lib/main.dart
+++ flutter_sample_01/lib/main.dart
@@ -4,6 +4,8 @@ main() {
   runApp(const MyFirstApp());
 }
 
+String heading = "WELCOME!";
+
 class MyFirstApp extends StatelessWidget {
   const MyFirstApp({super.key});
 
@@ -24,10 +26,131 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[100],
       appBar: AppBar(
         title: const Text('My first app'),
       ),
-      backgroundColor: Colors.blue[100],
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.yellow,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    heading,
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () => print('button clicked'),
+                        child: const Text('Click me!'),
+                      ),
+                      IconButton(
+                        onPressed: () => print('icon button clicked'),
+                        icon: const Icon(Icons.mic),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () => print('elevated button clicked'),
+                    child: const Text('Click me!'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.red,
+                  border: Border.all(color: Colors.black, width: 2.0)),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      // color: Colors.yellow,
+                      width: 130,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.yellow,
+                          border: Border.all(color: Colors.black, width: 2.0)),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.yellow,
+                        border: Border.all(color: Colors.black, width: 2.0),
+                      ),
+                      width: 130,
+                      height: 200,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.yellow,
+                        border: Border.all(color: Colors.black, width: 2.0),
+                      ),
+                      width: 130,
+                      height: 200,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.orange,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    heading,
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () => print('button clicked'),
+                        child: const Text('Click me!'),
+                      ),
+                      IconButton(
+                        onPressed: () => print('icon button clicked'),
+                        icon: const Icon(Icons.mic),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () => print('elevated button clicked'),
+                    child: const Text('Click me!'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
