4033127064fce3513d9848158a9967db4f49b395
Jafin Jahfar <jafinjahfar@gmail.com>
2023-05-31 15:41:06 +0530

    flutter counter app

---
+++ flutter_counter_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stateful/screen_home/index.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      title: 'Flutter Demo',
+      home: ScreenHome(),
+    );
+  }
+}
